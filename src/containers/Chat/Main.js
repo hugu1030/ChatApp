f3cedae9c9a042974b8f49836691c82dbc48900c
hugu1030 <kelodon1030@ymobile.ne.jp>
2020-01-19 12:31:54 +0900

    #1_modify_database_list_display

---
+++ src/containers/Chat/Main.js
@@ -0,0 +1,7 @@
+import ChatTemplate from '../../templates/Chat';
+import { bindActionCreators, compose } from 'redux';
+import { connect } from 'react-redux';
+import { actions } from '../../modules/chat/index';
+
+
+
