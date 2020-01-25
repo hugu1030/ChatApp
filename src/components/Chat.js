b1e8cedc40e1c8d64762b855fff3553ef89195b9
hugu1030 <kelodon1030@ymobile.ne.jp>
2020-01-26 00:06:26 +0900

    feature/#8_create_Chat_Page

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,8 +4,28 @@ import '../style/Chat.css';
 
 class Chat extends React.Component {
 
+    componentDidMount() {
+        const database = firebase.database()
+        let selectedRoomNumLog = database.ref("selectedRoomNum")
+        let selectedRoomNum = 0
+        selectedRoomNumLog.on("value", (snapshot) => {
+            snapshot.forEach((children) => {
+                if (children.val().user == this.state.user)
+                {
+                    selectedRoomNum = children.val().selectedRoomNum
+                }
+                console.log(selectedRoomNum)
+            })
+        })
+        this.props.RoomNumSelecter(selectedRoomNum)
+    }
+
     render() {
         const currentUser = firebase.auth().currentUser;
+        const database = firebase.database()
+        const ChatLog = database.ref(this.props.selectedRoomNum)
+        console.log(this.props.state.selectedRoomNum)
+
         return (
             <div className="chatPage">
                 <div className="userNamePosition"><a className="userName">User:{this.props.user}</a></div>
@@ -13,12 +33,13 @@ class Chat extends React.Component {
                 <input type="button" className="chatButton" onClick={() => this.props.addEventListener()} value="send" />
                 <input type="button" className="chatLogout" onClick={() => this.props.logout()} value="logout" />
                 <div className="chatDisplayArea">
-
-                    {this.props.log.map((chat, index) => {
-                        console.log(chat)
-                        return (
-                            (chat.user == currentUser.displayName) ? (<div className="currentUser"><div className="userPhotoPosition"><img src={chat.photo} className="userPhoto" /></div><div className="currentUserMessagePosition"><p className="currentUserMessage">{chat.message}</p></div> <div className="currentUserNamePosition"><p className="currentUserName">{chat.user}</p></div></div>) : (<div className="others"><div className="othersPhotoPosition"><img src={chat.photo} className="othersPhoto" /></div><div className="othersMessagePosition"><p className="othersMessage">{chat.message}</p></div> <div className="othersNamePosition"><p className="othersName">{chat.user}</p></div></div>)
-                        )
+                    {ChatLog.on("value", (snapshot) => {
+                        snapshot.forEach((children) => {
+                            return (
+                                (children.val().user == currentUser.displayName) ? (<div className="currentUser"><div className="userPhotoPosition"><img src={children.val().photo} className="userPhoto" /></div><div className="currentUserMessagePosition"><p className="currentUserMessage">{children.val().message}</p></div> <div className="currentUserNamePosition"><p className="currentUserName">{children.val().user}</p></div></div>) : (<div className="others"><div className="othersPhotoPosition"><img src={children.val().photo} className="othersPhoto" /></div><div className="othersMessagePosition"><p className="othersMessage">{children.val().message}</p></div> <div className="othersNamePosition"><p className="othersName">{children.val().user}</p></div></div>)
+                            )
+                        })
+                    })
                     })}
                 </div>
             </div>
--- src/templates/Home.js
+++ src/templates/Home.js
@@ -4,8 +4,7 @@ import firebase from '../firebase/config.js';
 import ChatPage from '../components/Chat.js';
 import ChatRoomTextBox from '../components/ChatRoomTextBox.js';
 import ChatRoomTextButton from '../components/ChatRoomTextButton.js';
-import '../style/Home.css'
-
+import '../style/Home.css';
 import {
     BrowserRouter as Router,
     Switch,
@@ -20,12 +19,12 @@ class Home extends React.Component {
             user: null,
             photo: null,
             message: '',
-            log: [],
+            //log: [],
             roomNumTotal: 0,
             selectedRoomNum: null,
             roomName: '',
             roomNameError: '',
-            roomArray: [],
+            //roomArray: [],
             doneMakeRoom: 0,
         }
     }
@@ -55,13 +54,27 @@ class Home extends React.Component {
             })
             return
         }
-        const i = 1
-        const ChatArrayInformation = this.state.roomArray.slice()
-        const ChatObject = { roomName: this.state.roomName, roomNum: this.state.roomNumTotal + i, roomNumTotal: this.state.roomNumTotal + 1 }
-        ChatArrayInformation.push(ChatObject)
-        console.log(ChatObject)
+
+        const database = firebase.database();
+        const RoomInformation = "RoomInformation"
+        const roomNumInfo = database.ref("roomNum");
+        let roomNum = 0
+        roomNumInfo.on("value", (snapshot) => {
+            snapshot.forEach((children) => {
+                roomNum = children.val().roomNum
+            })
+        })
+        database.ref(RoomInformation).push({
+            roomName: this.state.roomName,
+            roomNum: roomNum + 1
+        });
+
+        // const ChatArrayInformation = this.state.roomArray.slice()
+        // const ChatObject = { roomName: this.state.roomName, roomNum: this.state.roomNumTotal + i, roomNumTotal: this.state.roomNumTotal + 1 }
+        // ChatArrayInformation.push(ChatObject)
+        // console.log(ChatObject)
         this.setState({
-            roomArray: ChatArrayInformation,
+            // roomArray: ChatArrayInformation,
             roomName: '',
             roomNumTotal: this.state.roomNumTotal + 1,
             roomNameError: ''
@@ -75,24 +88,33 @@ class Home extends React.Component {
     }
 
     ClickChatRoom = (num) => {
+        const database = firebase.database()
+        database.ref("selectedRoomNum").push({
+            selectedRoomNum: num,
+            user: this.state.user
+        })
+        database.ref("doneMakeRoom").push({
+            doneMakeRoom: 1,
+            user: this.state.user,
+        })
+
         this.setState({
             doneMakeRoom: 1,
             selectedRoomNum: num
         })
     }
 
-    RoomNumSelecter = () => {
+    RoomNumSelecter = (num) => {
         this.setState({
-            selectedRoomNum: 1
+            selectedRoomNum: num
         })
-        console.log("RoomNumSelecter")
     }
 
-    handleDoneMakeRoom = () => {
+    /* handleDoneMakeRoom = () => {
         this.setState({
             doneMakeRoom: 1
         })
-    }
+    } */
 
     login = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
@@ -125,11 +147,19 @@ class Home extends React.Component {
     }
 
     addEventListener = () => {
-        const room = "chat"
         const database = firebase.database();
-        console.log(this.state.selectedRoomNum)
-        //console.log(this.state.)
-        database.ref(this.state.selectedRoomNum).push({
+        const selectedRoomNumLog = database.ref("selectedRoomNum")
+        let selectedRoomNum = 0
+        selectedRoomNumLog.on("value", (snapshot) => {
+            snapshot.forEach((children) => {
+                if (children.val().user == this.state.user)
+                {
+                    selectedRoomNum = children.val().selectedRoomNum
+                }
+            })
+        })
+
+        database.ref('selectedRoomNum').push({
             user: this.state.user,
             message: this.state.message,
             photo: this.state.photo
@@ -139,12 +169,13 @@ class Home extends React.Component {
             message: '',
         })
 
-        const chatLog = firebase.database().ref(this.state.selectedRoomNum);
+        const chatLog = firebase.database().ref('selectedRoomNum');
 
         chatLog.on("value", (snapshot) => {
             this.setState({
                 log: []
             })
+
             const logarray = []
             snapshot.forEach((children) => {
                 const logchild = { user: children.val().user, message: children.val().message, photo: children.val().photo }
@@ -155,7 +186,6 @@ class Home extends React.Component {
             })
         })
     }
-
     componentDidMount() {
         firebase.auth().onAuthStateChanged(
             (user) => {
@@ -178,44 +208,61 @@ class Home extends React.Component {
     }
 
     render() {
+        const database = firebase.database()
+        database.ref("roomNum").push({
+            roomNum: this.state.roomNumTotal,
+        })
+
         const PageLink = [];
         const PageRouter = [];
-        let roomNumTotal = this.state.roomNumTotal
-
-        for (let i = 0; i < roomNumTotal; i++)
-        {
-            PageLink.push(< Link to='roomNumTotal' onClick={() => this.ClickChatRoom(roomNumTotal)}><div className="ChatRoomLinkBox" >Room名:{this.state.roomArray[i].roomName}</div></Link>)
-            PageRouter.push(<Route path='roomNumTotal'><ChatPage {...this.state} /></Route>)
-        }
+        const roomChatLog = database.ref("RoomInformation")
+        roomChatLog.on("value", (snapshot) => {
+            snapshot.forEach((children) => {
+                PageLink.push(< Link to='children.val().roomNum' onClick={() => this.ClickChatRoom(children.val().roomNum)}><div className="ChatRoomLinkBox" >Room名:{children.val().roomName}</div></Link>)
+                PageRouter.push(<Route path='children.val().roomNum'><ChatPage {...this.state} /></Route>)
+            })
+        })
         /* console.log(this.state.roomName)
          console.log(this.state.roomNumTotal)
          console.log(this.state.doneMakeRoom)
          console.log(this.state.selectedRoomNum) */
+        const doneMakeLog = database.ref("doneMakeRoom")
+        let doneMakeRoom = 0
+        doneMakeLog.on("value", (snapshot) => {
+            snapshot.forEach((children) => {
+                if (children.val().user == this.state.user)
+                {
+                    doneMakeRoom = children.val().doneMakeRoom
+                }
+            })
+        })
+
         return (
             <div class="chatAll">
                 {
                     this.state.user ?
-                        (this.state.doneMakeRoom == 1 ? (<ChatPage {...this.state} addEventListener={this.addEventListener} handleMessage={this.handleMessage} logout={this.logout} />) :
-                            (<div className="ChatRoomContainer">
-                                <h1>Enter Chat Name</h1>
-                                <div className="ChatRoomTextBox">
-                                    <ChatRoomTextBox  {...this.state} RoomNameHandler={this.RoomNameHandler} />
-                                </div>
-                                <div className="ChatRoomTextButtonP">
-                                    <ChatRoomTextButton RoomSetter={this.RoomSetter} />
-                                </div>
-                                <div className="ErrorMessage">
-                                    {this.state.roomNameError}
-                                </div>
-                                <Router>
-                                    <div className="ChatRoomLinkContainer">
-                                        {PageLink}
+                        (doneMakeRoom == 1 ? (<ChatPage {...this.state} addEventListener={this.addEventListener} handleMessage={this.handleMessage} logout={this.logout} RoomNumSelecter={this.RoomNumSelecter} />) :
+                            (
+                                <div className="ChatRoomContainer">
+                                    <h1>Enter Chat Name</h1>
+                                    <div className="ErrorMessage">
+                                        {this.state.roomNameError}
                                     </div>
-                                    <Switch>
-                                        {PageRouter}
-                                    </Switch>
-                                </Router>
-                            </div>
+                                    <div className="ChatRoomTextBox">
+                                        <ChatRoomTextBox  {...this.state} RoomNameHandler={this.RoomNameHandler} />
+                                    </div>
+                                    <div className="ChatRoomTextButtonP">
+                                        <ChatRoomTextButton RoomSetter={this.RoomSetter} />
+                                    </div>
+                                    <Router>
+                                        <div className="ChatRoomLinkContainer">
+                                            {PageLink}
+                                        </div>
+                                        <Switch>
+                                            {PageRouter}
+                                        </Switch>
+                                    </Router>
+                                </div>
 
                             )) : (<Login {...this.state} logout={this.logout} login={this.login} />
                         )
